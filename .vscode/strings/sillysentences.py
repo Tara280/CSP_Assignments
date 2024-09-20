a29a49cdb7d984df3a52c3eb2b723043f030c686
Tara280 <tara.tabla@ucas-edu.net>
2024-09-20 19:18:40 +0000

    created

---
+++ .vscode/strings/sillysentences.py
@@ -0,0 +1,9 @@
+animal = input("Name an animal: ")
+place = input("Name a place: ")
+verb = input("Name a verb: ")
+
+
+print("The " ,animal)
+print("went to" ,place)
+print("and it" ,verb)
+print("with their friend the fat parrot.")
