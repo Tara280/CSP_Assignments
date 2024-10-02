11410b24e9339bd94748f5f1fe804e265d273f69
Tara280 <tara.tabla@ucas-edu.net>
2024-10-02 19:36:22 +0000

    added stuff

--- .vscode/conditionals/time.c
+++ .vscode/conditionals/time.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <time.h>
 
+int hour;
+
 int main()
 {
     time_t rawtime;
@@ -9,6 +11,15 @@ int main()
     time(&rawtime); 
     timeinfo = localtime (&rawtime);
     printf("Current local time and date: %s", asctime(timeinfo));
+    printf("Tell me the hour in military time!:\n");
+    scanf("%d", &hour);
+    if (hour < 12){
+        printf("Good Morning\n");
+    }else if(hour < 18){
+        printf("Good Afternoon\n");
+    }else{
+        printf("Good Evening\n");
+    }
 
     return 0;
 }
