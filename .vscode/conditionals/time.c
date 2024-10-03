d13591ca5319da32ee3d3944da62985bb0537893
Tara280 <tara.tabla@ucas-edu.net>
2024-10-03 19:04:15 +0000

    fixed the beginning segment

--- .vscode/conditionals/time.c
+++ .vscode/conditionals/time.c
@@ -5,14 +5,14 @@ int hour;
 
 int main()
 {
-    time_t rawtime;
-    struct tm * timeinfo;
+    time_t now;
+    struct tm * now_tm;
 
-    time(&rawtime); 
-    timeinfo = localtime (&rawtime);
-    printf("Current local time and date: %s", asctime(timeinfo));
-    printf("Tell me the hour in military time!:\n");
-    scanf("%d", &hour);
+    now = time(NULL); 
+    now_tm = localtime(&now);
+    hour = (now_tm->tm_hour);
+    printf("%d\n", hour);
+
     if (hour < 12){
         printf("Good Morning\n");
     }else if(hour < 18){
