6ee8c320721c47749cfedf65d3470ba13827aa87
Tara280 <tara.tabla@ucas-edu.net>
2024-10-11 19:05:47 +0000

    adjusted code

--- .vscode/loops/fiz_buzz.c
+++ .vscode/loops/fiz_buzz.c
@@ -9,9 +9,9 @@ int main(){
     printf("Give me a number\n");
     scanf("%d", &usr);
     printf("Give me a short word\n");
-    scanf("%s", &one);
+    scanf("%s", one);
     printf("Give me a another short word\n");
-    scanf("%s", &two);
+    scanf("%s", two);
     strcat(three, one);
     strcat(three, two);
     //loop that counts to 50
