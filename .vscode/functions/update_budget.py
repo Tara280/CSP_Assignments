6fc8e39d8c5456f9501b9646ed7d640a6949f118
Tara280 <tara.tabla@ucas-edu.net>
2024-10-21 19:39:12 +0000

    fixed it

--- .vscode/functions/update_budget.py
+++ .vscode/functions/update_budget.py
@@ -16,15 +16,13 @@ savings = income*0.2
 expenses = rent + utilities + groceries + transportation
 spend = income-int(expenses)-savings
 
-prent = rent/income*100
-putilities = utilities/income *100
-pgroceries = groceries/income *100
-ptransportation = transportation/income *100
-psavings = savings/income *100
-pexpenses = expenses/income *100
-print(f"Your rent is {int(prent)}% of your monthly income\n")
-print(f"Your utilities is {int(putilities)}% of your monthly income\n")
-print(f"Your groceries is {int(pgroceries)}% of your monthly income\n")
-print(f"Your transportation is {int(ptransportation)}% of your monthly income\n")
-print(f"Your savings is {int(psavings)}% of your monthly income\n")
-print(f"Your expenses is {int(pexpenses)}% of your monthly income\n")
+def percent(type,amount):
+    per = amount / income *100
+    return(f"Your {type} is {per}% income")
+
+print(percent("rent percentage", rent))
+print(percent("utilities percentage", utilities))
+print(percent("groceries percentage", groceries))
+print(percent("transportation percentage", transportation))
+print(percent("expenses percentage", expenses))
+print(percent("spending percentage", spend))
